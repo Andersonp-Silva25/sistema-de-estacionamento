e7e85cac01c8941d126a78128639e57f9336f35f
Andersonp-Silva25 <andersonp.silva25@gmail.com>
2023-12-02 19:44:09 -0300

    :sparkles:Feat: Listando todos os veiculos cadastrados

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -62,6 +62,10 @@ namespace DesafioFundamentos.Models
                 Console.WriteLine("Os veículos estacionados são:");
                 // TODO: Realizar um laço de repetição, exibindo os veículos estacionados
                 // *IMPLEMENTE AQUI*
+                foreach (string elemento in veiculos)
+                {
+                    Console.WriteLine(elemento);
+                }
             }
             else
             {
